2390cd61c6a7d6c7acb5e2af8a0d82d86515f6a6
Mark Pilgrim <mark@diveintomark.org>
2009-12-17 11:14:01 -0500

    fixed default colors

--- defaults.js
+++ defaults.js
@@ -1,8 +1,8 @@
 default_generic_fonts = true;
 default_colors_option = "custom";
-default_custom_text_color = "000000";
-default_custom_background_color = "ffffff";
+default_custom_text_color = "#000000";
+default_custom_background_color = "#FFFFFF";
 default_underline_links = true;
 default_link_colors_option = "custom";
-default_custom_link_color = "0000ff";
-default_custom_visited_color = "800080";
+default_custom_link_color = "#0000FF";
+default_custom_visited_color = "#800080";
